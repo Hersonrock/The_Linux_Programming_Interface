b4d91d8f02386074e309e9858491ffa74ebba8e3
Hersonrock <hjpg20@gmail.com>
2024-05-23 21:40:04 -0500

    Adds finished kill_parent program where I was testing interactions with orphaned processes

--- ch6/kill_parent.c
+++ ch6/kill_parent.c
@@ -3,17 +3,23 @@
 #include<signal.h>
 
 int main(int argc, char *argv[]){
-        printf("This process ID is %d\n",getpid());
-        printf("Sleeping...\n");
-        sleep(10);
-        printf("killing process parent with ID %d\n", getppid());
-        if (kill(getppid(),9) != 0){
+        pid_t parentPID,childPID;
+        
+        parentPID = getpid();
+        printf("This process PPID is %d\n", getppid());
+        printf("This process PID is %d\n",parentPID);
+        //printf("killing process parent with ID %d\n", getppid());
+        childPID = fork(); 
+        printf("Child PID is %d\n", childPID);
+        if (kill(parentPID,15) != 0){
                 perror("Failed to Kill parent\n");
         }
         else{
                 printf("Parent killed succesfully\n");
         }
-        printf("This process parent now has ID %d\n", getppid());
+        printf("The current PPID is %d\n", getppid());        
         sleep(100);
+        printf("Sleep over, exiting pid %d, with ppid %d\n", getpid(), 
+                getppid());
         return 0;
 }
