b2a7e5823e3dee4b22483ff945bc8312237531c0
Hersonrock <hjpg20@gmail.com>
2024-05-25 23:34:46 -0500

    Started chapter 7 and made a project looking to understand brk()

---
+++ ch7/brk1.c
@@ -0,0 +1,27 @@
+#include<stdio.h>
+#include<unistd.h>
+#include<string.h>
+#include<stdlib.h>
+
+extern char etext, edata, end;
+
+int main(int argc, char *argv[]){
+        char *heapStart = sbrk(0);
+        sbrk(8);
+        char *heapEnd = sbrk(0);
+        //strcpy(heapStart, "abcdefg\0");
+
+        printf("PID:%d\n", getpid());
+        
+        printf("Final   brk= %p\n", heapEnd);
+        printf("Initial brk= %p\n", heapStart);
+        printf("BSS end      %p\n", &end);
+        printf("edata end    %p\n", &edata);
+        printf("etext end    %p\n", &etext);
+        //printf("This is the content of heap: %s\n", heapStart);
+        
+        while(1){
+        }
+
+        return 0;
+}
