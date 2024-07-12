785eb2fd4f291505ced20537a3ce5135a8146c8e
Hersonrock <hjpg20@gmail.com>
2024-07-12 08:29:31 -0500

    Todo1  completed, file can be opened and offset can be changed

--- ch5/dupfdcheck/src/main.c
+++ ch5/dupfdcheck/src/main.c
@@ -2,9 +2,29 @@
 #include<stdlib.h>
 #include<fcntl.h>
 #include<unistd.h>
+#include<sys/types.h>
 
-
+#define INPUT_PATH "./inputFile"
+#define MOVE 10
 int main(int argc, char *argv[]){
+        int fd;
+        //mode_t mode = S_IRUSR | S_IRGRP | S_IROTH;
+        off_t  curr, new_off;
+
+        //int open(const char *pathname, int flags, ...
+        /* mode_t mode */ 
+        fd = open(INPUT_PATH, O_RDONLY); 
+        if(fd == -1){
+                fprintf(stderr, "Failed open file %s\n", INPUT_PATH);
+                return EXIT_FAILURE;
+        }
+        printf("File %s opened succefully\n", INPUT_PATH);
+        curr = lseek(fd, 0 , SEEK_CUR);
+        printf("File initial offset: %ld\n", curr);
+        new_off = lseek(fd, MOVE , SEEK_SET);
+        printf("Moving offset +%d bytes\n", MOVE);
+        printf("File current offset: %ld\n", new_off);
+
 
         return EXIT_SUCCESS;
 }
