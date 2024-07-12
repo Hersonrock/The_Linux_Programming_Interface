47069d1c81233682ae195a5d62883c58ef67f083
Hersonrock <hjpg20@gmail.com>
2024-07-12 08:41:24 -0500

    Completing to-do 2-3, Duplicated fd and offset checking completed

--- ch5/dupfdcheck/src/main.c
+++ ch5/dupfdcheck/src/main.c
@@ -7,9 +7,9 @@
 #define INPUT_PATH "./inputFile"
 #define MOVE 10
 int main(int argc, char *argv[]){
-        int fd;
+        int fd, dup_fd;
         //mode_t mode = S_IRUSR | S_IRGRP | S_IROTH;
-        off_t  curr, new_off;
+        off_t  curr, new_off, dup_curr;
 
         //int open(const char *pathname, int flags, ...
         /* mode_t mode */ 
@@ -18,13 +18,18 @@ int main(int argc, char *argv[]){
                 fprintf(stderr, "Failed open file %s\n", INPUT_PATH);
                 return EXIT_FAILURE;
         }
-        printf("File %s opened succefully\n", INPUT_PATH);
+        printf("File %s opened succefully, fd: %d\n", INPUT_PATH, fd);
         curr = lseek(fd, 0 , SEEK_CUR);
         printf("File initial offset: %ld\n", curr);
         new_off = lseek(fd, MOVE , SEEK_SET);
         printf("Moving offset +%d bytes\n", MOVE);
         printf("File current offset: %ld\n", new_off);
 
+        dup_fd = dup(fd);
+        printf("File descriptor duplicated, original: %d, duplicate: %d\n",
+                        fd, dup_fd);
+        dup_curr = lseek(dup_fd, 0 , SEEK_CUR);
+        printf("Duplicated fd offset: %ld\n", dup_curr);
 
         return EXIT_SUCCESS;
 }
