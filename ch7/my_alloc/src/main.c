ef27065aa44189b76dd61dd299590284e7c6e7e5
Hersonrock <hjpg20@gmail.com>
2024-08-10 14:59:47 -0500

    Changing ints to use actual 8byte long integers, Including the header to be 8 bytes

--- ch7/my_alloc/src/main.c
+++ ch7/my_alloc/src/main.c
@@ -2,12 +2,13 @@
 #include<stdlib.h>
 #include<unistd.h>
 #include<string.h>
+#include<stdint.h>
 
 #define ALIGN_SIZE 16
 #define HEADER_SIZE ALIGN_SIZE / 2
 #define SIZE 5
 
-int get_block_size(int in_size);
+int64_t get_block_size(int64_t in_size);
 void *my_alloc(size_t size);
 int main(int argc, char *argv[]){
         char *heap_start = sbrk(0);
@@ -21,7 +22,7 @@ int main(int argc, char *argv[]){
                 payload_ptr[i] = i + 1;
         }
 
-        //printing values
+        //print64_ting values
         for(size_t i = 0; i < SIZE; i++){
                 printf("Payload[%ld] = %d\n", i, payload_ptr[i]);
         }
@@ -30,16 +31,17 @@ int main(int argc, char *argv[]){
         brk(heap_start);
         return EXIT_SUCCESS;
 }
-int get_block_size(int in_size){
+int64_t get_block_size(int64_t in_size){
         in_size += HEADER_SIZE;
-        int size = (in_size / ALIGN_SIZE) * ALIGN_SIZE;
+        int64_t size = (in_size / ALIGN_SIZE) * ALIGN_SIZE;
         if(in_size % ALIGN_SIZE) size += ALIGN_SIZE;
+        printf("size= %ld\n", size);
         return size;
 }
 
 void *my_alloc(size_t size){
-        int block_size = get_block_size(size);
-        int header = block_size | 0x1;
+        int64_t block_size = get_block_size(size);
+        int64_t header = block_size | 0x1;
         char *blk_ptr = sbrk(block_size);
 
         memcpy(blk_ptr, &header, sizeof(header));
